d963a0fd6be60fa7cfc3838356e0e85bfb7f9b23
FidelinoMa.Donnna <mariadelossantos207@gmail.com>
2023-03-09 23:44:09 +0800

    My Educational Background added

--- git-python-Fidelino.py
+++ git-python-Fidelino.py
@@ -1,3 +1,6 @@
 print("Ma. Donna D. Fidelino")
 print("Tigwi Torrijos Marinduque")
 print("January 19, 2003")
+print("""Marinduque State College
+      Bachelor of Science in Information Technology-2A
+      """)
