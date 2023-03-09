82ab9c83d88ed57c90ca0390444eb1b8563c3f73
FidelinoMa.Donnna <mariadelossantos207@gmail.com>
2023-03-09 23:57:54 +0800

    My Hobbies Added

--- git-python-Fidelino.py
+++ git-python-Fidelino.py
@@ -3,4 +3,7 @@ print("Tigwi Torrijos Marinduque")
 print("January 19, 2003")
 print("""Marinduque State College
       Bachelor of Science in Information Technology-2A
-      """)
+      
+      Hobbies:
+              Playing Softball
+              watching Anime and Disney movies""")
